bfc221fed734b7cbb8c6c91cfd4d5c718b1f24a5
Mammadtaghi <tu7k3zlya@code.edu.az>
2023-11-10 14:32:32 +0400

    nav sideNav Completed

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// Toggle Icon Fucntion
+
+// HTML stuff
+const overlay = document.querySelector('.overlay')
+const toggleIcon = document.querySelector('.toggleIcon')
+const sideNavContainer = document.querySelector('.sideNavContainer')
+
+function openSideNav() {
+    sideNavContainer.style.left = '0px'
+    overlay.style.zIndex = '0'
+}
+
+// Side Nav Close Button Function
+
+// HTML stuff
+const sideNavCloseBtn = document.querySelector('.sideNavCloseBtn')
+
+function closeSideNav() {
+    sideNavContainer.style.left = '-400px'
+    overlay.style.zIndex = '-2'
+}
+
+// Side Shop SubNav Opening and Closing
+
+// HTML stuff
+const sideShopSubNav = document.querySelector('.sideShopSubNav')
+const sideNavShop = document.querySelector('.sideNavShop')
+const shopNavTitle = document.querySelector('.shopNavTitle')
+
+function openShop() {
+    sideShopSubNav.style.right = '0px'
+}
+
+function closeShop() {
+    sideShopSubNav.style.right = '-400px'
+}
+
+
+// Side Pages SubNav Opening and Closing
+
+// HTML stuff
+const sidePagesSubNav = document.querySelector('.sidePagesSubNav')
+const sideNavPages = document.querySelector('.sideNavPages')
+const pagesNavTitle = document.querySelector('.pagesNavTitle')
+
+function openPages() {
+    sidePagesSubNav.style.right = '0px'
+}
+
+function closePages() {
+    sidePagesSubNav.style.right = '-400px'
+}
+
+
+
+
